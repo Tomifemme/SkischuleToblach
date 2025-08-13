8f147672f6e655d29176a892e5ec7abf48e7beb3
Tomi Nieder <tomifemme@gmail.com>
2025-08-13 18:14:44 +0000

    either is not showing the Dobbiaco forecast

--- src/app/(main)/weather/page.tsx
+++ src/app/(main)/weather/page.tsx
@@ -7,7 +7,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 export default function WeatherPage() {
   const { t } = useTranslation();
 
-  const weatherWidgetUrl = "https://www.meteoblue.com/en/weather/widget/daily/dobbiaco_italy_3178003?geoloc=fixed&days=5&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uvindex=0&uvindex=1&humidity=0&humidity=1&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&spot=1&pressure=0&pressure=1&sunshine=0&sunshine=1&moonphase=0&sunrise=0&flowunit=METRE_PER_SECOND&forecast_length=5&background=https:////cdn.meteoblue.com/website/images/2022/08/bg-colors-3-small.jpg&foreground=FFFFFF&shadow=0&textcontrast=2&textcolor=FFFFFF&textcolor1=FFFFFF&textcolor2=FFFFFF";
+  const weatherWidgetUrl = "https://www.meteoblue.com/en/weather/widget/daily/dobbiaco_italy_3178003?geoloc=fixed&days=5&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=1&maxtemperature=1&mintemperature=1&windspeed=1&windgust=0&winddirection=1&uvindex=1&humidity=1&precipitation=1&precipitationprobability=1&spot=1&pressure=1&sunshine=1";
 
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
